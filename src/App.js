681ff118c78aa4f87e72e0b762679acae867d518
Nagendra <nagindra98@gmail.com>
2026-03-14 18:57:48 +0530

    Revert "handle delete key"
    This reverts commit 82fdfa8736d179c6018c36f36340ef406c4a84f0.

--- src/App.js
+++ src/App.js
@@ -56,12 +56,6 @@ export default function App() {
     setSegmentStart(cursorPos + value.length);
   };
 
-  const handleEnter = () => {
-    commitSegment();
-    insertAtCursor("\n");
-    return;
-  }
-
   const handleBackspace = () => {
     if (cursorPos === 0) return;
     setText(prev => {
@@ -75,35 +69,9 @@ export default function App() {
     setSegmentStart(newPos);
   };
 
-  const handleDelete = () => {
-    if (cursorPos === 0) return;
-    setText(prev => {
-      const before = prev.slice(0, cursorPos);
-      const after = prev.slice(cursorPos + 1);
-      return after ? (before + after) : before;
-    });
-    const newPos = cursorPos + 1;
-    setCursorPos(newPos);
-    setLatinBuffer("");
-    setSegmentStart(newPos);
-  };
-
   const handleTyping = (e) => {
     if (!phonetic) return;
     const key = e.key;
-
-    if (key === "Enter") {
-      e.preventDefault();
-      handleEnter();
-      return;
-    }
-
-    if (key === "Delete") {
-      e.preventDefault();
-      handleDelete();
-      return;
-    }
-
     if (
       key === "Shift" ||
       key === "Control" ||
@@ -111,11 +79,11 @@ export default function App() {
       key === "Meta" ||
       key === "CapsLock" ||
       key === "Tab" ||
+      key === "Enter" ||
       key === "ArrowRight" ||
       key === "ArrowLeft" ||
       key === "ArrowDown" ||
-      key === "ArrowUp" ||
-      key === "Home"
+      key === "ArrowUp"      
     ) {
       return;
     }
